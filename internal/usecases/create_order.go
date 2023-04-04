dc71c1b893f78f648597833731f96467f040db8a
paulo gabriel <pgabrieldeveloper@outlook.com>
2023-04-04 17:55:06 -0300

    feat implementando metodo execute

--- internal/usecases/create_order.go
+++ internal/usecases/create_order.go
@@ -1,37 +1,61 @@
-package usecases
+package usecase
 
 import (
 	"github.com/PGabrielDev/clean-archi-go/internal/entity"
-	events2 "github.com/PGabrielDev/clean-archi-go/internal/events"
 	"github.com/PGabrielDev/clean-archi-go/pkg/events"
 )
 
 type OrderInputDTO struct {
-	ID    string  `json:"ID"`
+	ID    string  `json:"id"`
 	Price float64 `json:"price"`
 	Tax   float64 `json:"tax"`
 }
 
 type OrderOutputDTO struct {
-	ID         string  `json:"ID"`
+	ID         string  `json:"id"`
 	Price      float64 `json:"price"`
 	Tax        float64 `json:"tax"`
-	FinalPrice float64 `json:"finalPrice"`
-}
-type CalculateFinalPriceUseCase struct {
-	Repository     entity.OrderRepositoryInterface
-	OrderEvent     events2.OrderCreatedEvent
-	EventDispacher events.EventDispatcherInterface
+	FinalPrice float64 `json:"final_price"`
 }
 
-func NewCalculateFinalPriceUseCase(repository entity.OrderRepositoryInterface, event events2.OrderCreatedEvent, eventD events.EventDispatcherInterface) *CalculateFinalPriceUseCase {
-	return &CalculateFinalPriceUseCase{
-		Repository:     repository,
-		OrderEvent:     event,
-		EventDispacher: eventD,
+type CreateOrderUseCase struct {
+	OrderRepository entity.OrderRepositoryInterface
+	OrderCreated    events.EventInterface
+	EventDispatcher events.EventDispatcherInterface
+}
+
+func NewCreateOrderUseCase(
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreated events.EventInterface,
+	EventDispatcher events.EventDispatcherInterface,
+) *CreateOrderUseCase {
+	return &CreateOrderUseCase{
+		OrderRepository: OrderRepository,
+		OrderCreated:    OrderCreated,
+		EventDispatcher: EventDispatcher,
 	}
 }
 
-func (c *CalculateFinalPriceUseCase) Execute(dto OrderInputDTO) (OrderOutputDTO, error) {
-	panic("")
+func (c *CreateOrderUseCase) Execute(input OrderInputDTO) (OrderOutputDTO, error) {
+	order := entity.Order{
+		ID:    input.ID,
+		Price: input.Price,
+		Tax:   input.Tax,
+	}
+	order.CalculateFinalPrice()
+	if err := c.OrderRepository.Save(&order); err != nil {
+		return OrderOutputDTO{}, err
+	}
+
+	dto := OrderOutputDTO{
+		ID:         order.ID,
+		Price:      order.Price,
+		Tax:        order.Tax,
+		FinalPrice: order.Price + order.Tax,
+	}
+
+	c.OrderCreated.SetPayload(dto)
+	c.EventDispatcher.Dispatch(c.OrderCreated)
+
+	return dto, nil
 }
