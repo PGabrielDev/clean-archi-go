2b67c92fe55896c80cd94e94d92ee13bff58b3f5
paulo gabriel <pgabrieldeveloper@outlook.com>
2023-04-07 00:14:06 -0300

    test: adicionando test de repositorio

--- internal/infra/database/order_repository_test.go
+++ internal/infra/database/order_repository_test.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"database/sql"
+	"github.com/PGabrielDev/clean-archi-go/internal/entity"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -30,6 +31,21 @@ func TestSuite(t *testing.T) {
 	suite.Run(t, new(OrderRepositoryTestSuite))
 }
 
-func (suite *OrderRepositoryTestSuite) TestCreateNewOrderAndGivenValueFinalPrice() {
+func (suite *OrderRepositoryTestSuite) TestGivenAnOrder_WhenSave_ThenShouldSaveOrder() {
+	order, err := entity.NewOrder("123", 10.0, 2.0)
+	suite.NoError(err)
+	suite.NoError(order.CalculateFinalPrice())
+	repo := NewOrderRepository(suite.Db)
+	err = repo.Save(order)
+	suite.NoError(err)
 
+	var orderResult entity.Order
+	err = suite.Db.QueryRow("Select id, price, tax, final_price from orders where id = ?", order.ID).
+		Scan(&orderResult.ID, &orderResult.Price, &orderResult.Tax, &orderResult.FinalPrice)
+
+	suite.NoError(err)
+	suite.Equal(order.ID, orderResult.ID)
+	suite.Equal(order.Price, orderResult.Price)
+	suite.Equal(order.Tax, orderResult.Tax)
+	suite.Equal(order.FinalPrice, orderResult.FinalPrice)
 }
